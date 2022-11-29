fd534a2abfb0fbed6fc284e9fc1ed01461af93ee
Matthew Wong <matthewwong525@gmail.com>
2022-11-29 11:08:07 -0500

    add regex to remove .md from links to make relative links functional

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,6 +2,9 @@ import { remark } from 'remark'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
+  // remove `.md` from links
+  markdown = markdown.replaceAll(/(\[[^\[\]]+\]\([^\(\)]+)(\.md)(\))/g, "$1$3");
+
   const result = await remark().use(html).process(markdown)
   return result.toString()
 }

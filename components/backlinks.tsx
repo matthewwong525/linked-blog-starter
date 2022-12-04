8ea3be89dd862631b7c9e9e82d1b3a4bd5b8d362
Matthew Wong <matthewwong525@gmail.com>
2022-12-04 12:15:20 -0500

    make backlinks clickable and adjust note-preview text sizing + border

--- components/backlinks.tsx
+++ components/backlinks.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import NotePreview from "./note-preview"
 
 type Props = {
@@ -12,12 +13,14 @@ type Props = {
 const Backlinks = ({ backlinks }: Props) => {
   return (
     <>
-      <h2 className="text-2xl">Backlinks</h2>
+      <h2 className="text-2xl pb-3">Backlinks</h2>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         {Object.keys(backlinks).map((slug) => {
           const post = backlinks[slug]
           return (
-            <NotePreview title={post.title} content={post.excerpt}/>
+            <Link href={slug} className="col-span-2">
+              <NotePreview title={post.title} content={post.excerpt} />
+            </Link>
           )
         })}
       </div>
--- components/note-preview.tsx
+++ components/note-preview.tsx
@@ -5,9 +5,9 @@ type Props = {
 
 const NotePreview = ({ title, content }: Props) => {
   return (
-    <span className="note-preview block col-span-2 max-w-[400px] rounded-md border-2 border-black p-1 bg-white">
-      <span className="block font-bold text-xl">{title}</span>
-      <span className="block font-normal text-lg">{content}</span>
+    <span className="note-preview block col-span-2 max-w-[400px] rounded-md border border-black p-1 bg-white cursor-pointer">
+      <span className="block font-bold text-lg">{title}</span>
+      <span className="block font-normal text-base">{content}</span>
     </span>
   )
 }

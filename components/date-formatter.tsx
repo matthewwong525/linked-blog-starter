5fbb0510e1559b834f67a0484df983c02365d73a
Matthew Wong <matthewwong525@gmail.com>
2022-12-04 17:14:45 -0500

    add try catch for date formatter ot handle invalid dates

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -5,8 +5,13 @@ type Props = {
 }
 
 const DateFormatter = ({ dateString }: Props) => {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  try {
+    const date = parseISO(dateString)
+    return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  } catch (e) {
+    console.warn(e);
+    return null
+  }
 }
 
 export default DateFormatter

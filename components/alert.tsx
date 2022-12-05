e10e00d984b46e1f100889bf4c1641d90cfd9ba9
Matthew Wong <matthewwong525@gmail.com>
2022-12-05 14:32:22 -0500

    update github repo URL

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,6 +1,5 @@
 import Container from './container'
 import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
 
 type Props = {
   preview?: boolean
@@ -31,7 +30,7 @@ const Alert = ({ preview }: Props) => {
             <>
               The source code for this blog is{' '}
               <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+                href={`https://github.com/matthewwong525/linked-blog-starter`}
                 className="underline hover:text-blue-600 duration-200 transition-colors"
               >
                 available on GitHub

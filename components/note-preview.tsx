bcabff400511b63d09a5d68773c088e33fac9040
Matthew Wong <matthewwong525@gmail.com>
2022-12-05 15:30:34 -0500

    make note-preview font appear normal in headings

--- components/note-preview.tsx
+++ components/note-preview.tsx
@@ -7,7 +7,7 @@ const NotePreview = ({ title, content }: Props) => {
   return (
     <span className="note-preview block col-span-2 max-w-[400px] rounded shadow-sm p-5 bg-white cursor-pointer text-lg hover:bg-gray-200 hover:border-transparent">
       <span className="block font-bold leading-snug tracking-tight truncate mb-1">{title}</span>
-      <span className="block text-gray-600 whitespace-pre-line max-h-[150px] truncate">{content}</span>
+      <span className="block font-normal text-gray-600 whitespace-pre-line max-h-[150px] truncate">{content}</span>
     </span>
   )
 }

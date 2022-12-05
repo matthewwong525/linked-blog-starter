bd33b74ca445fd6b0ffe5491530450e51514f530
Matthew Wong <matthewwong525@gmail.com>
2022-12-05 13:55:23 -0500

    improve note-preview aesthetics

--- components/note-preview.tsx
+++ components/note-preview.tsx
@@ -5,9 +5,9 @@ type Props = {
 
 const NotePreview = ({ title, content }: Props) => {
   return (
-    <span className="note-preview block col-span-2 max-w-[400px] max-h-[200px] rounded-md border border-black p-1 bg-white cursor-pointer truncate">
-      <span className="block font-bold text-lg truncate">{title}</span>
-      <span className="block font-normal text-bas whitespace-pre-line">{content}</span>
+    <span className="note-preview block col-span-2 max-w-[400px] rounded shadow-sm p-5 bg-white cursor-pointer text-lg hover:bg-gray-200 hover:border-transparent">
+      <span className="block font-bold leading-snug tracking-tight truncate mb-1">{title}</span>
+      <span className="block text-gray-600 whitespace-pre-line max-h-[150px] truncate">{content}</span>
     </span>
   )
 }

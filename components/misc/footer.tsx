bbdf78e42cd026a22bdd5041c244ea52587cfdc1
Matthew Wong <matthewwong525@gmail.com>
2022-12-11 09:30:29 -0500

    add preview link to footer

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -1,10 +1,12 @@
+import PreviewLink from "./preview-link"
+
 const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            <PreviewLink href="/notes/statically-generated">Statically Generated</PreviewLink> with Next.js.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a

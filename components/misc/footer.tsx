6b89bdde3a9304238fdcd286a6d351c9d3b3046a
Matthew Wong <matthewwong525@gmail.com>
2022-12-12 12:26:36 -0500

    update footer

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            <PreviewLink href="/notes/statically-generated">Statically Generated</PreviewLink> with Next.js.
+            <PreviewLink href="/statically-generated">Statically Generated</PreviewLink> with Next.js.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
